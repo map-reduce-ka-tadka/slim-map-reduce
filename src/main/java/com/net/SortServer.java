0ea3a0f4b03ad88e26c5a8074164f35b0f6074fa
Abhijeet Sharma <sharma.abhi@husky.neu.edu>
2016-04-16 22:32:49 -0400

    included exceptions and comments in code

--- src/main/java/com/net/SortServer.java
+++ src/main/java/com/net/SortServer.java
@@ -21,36 +21,46 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class SortServer {
 
-	public static void start() {
-
+	public static void start() throws InterruptedException {
+		// configure the server
 		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		try {
-			ServerBootstrap boot = new ServerBootstrap()
-					.group(bossGroup, workerGroup)
-					.channel(NioServerSocketChannel.class)
-					.childHandler(new ChannelInitializer<SocketChannel>() {
-						@Override
-						public void initChannel(SocketChannel channel) throws Exception {
-							channel.pipeline().addLast(new SortServerInitializer());
-						}
-					})
-					.option(ChannelOption.SO_BACKLOG, 128)
-					.childOption(ChannelOption.SO_KEEPALIVE, true)
-					.childOption(ChannelOption.TCP_NODELAY, true);
+			ServerBootstrap boot = new ServerBootstrap();
+			boot.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.childHandler(new ChannelInitializer<SocketChannel>() {
+					@Override
+					public void initChannel(SocketChannel sChannel) throws Exception {
+						sChannel.pipeline().addLast(new SortServerInitializer());
+					}
+				})
+				.option(ChannelOption.SO_BACKLOG, 128)
+				.childOption(ChannelOption.SO_KEEPALIVE, true)
+				.childOption(ChannelOption.TCP_NODELAY, true);
+			
 			// Start the server.
-			ChannelFuture f = boot.bind(new InetSocketAddress(ConfigParams.MASTER_ADDRESS, ConfigParams.PORT)).sync();           	
+			ChannelFuture f = boot.bind(new InetSocketAddress(ConfigParams.MASTER_ADDRESS, ConfigParams.PORT)).sync();
+			System.out.println("Server started at " + ConfigParams.MASTER_ADDRESS + ":" + ConfigParams.PORT);
+			
 			// Wait until the server socket is closed.
 			f.channel().closeFuture().sync();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
-		} finally {
+		} 
+		finally {			
+			// Shut down all event loops to terminate all threads.
 			bossGroup.shutdownGracefully();
 			workerGroup.shutdownGracefully();
+			
+			// Wait until all threads are terminated.
+			bossGroup.terminationFuture().sync();
+			workerGroup.terminationFuture().sync();
 		}
 	}
-	public static void main(String[] args) {
-		new  SortServer();
+	
+	public static void main(String[] args) throws Exception {
 		SortServer.start();
 	}
 }
