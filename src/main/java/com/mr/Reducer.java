8d938802374b8357dc76722fdcfc3e6c1ce8af50
mehhtadeepen <mehta.de@husky.neu.edu>
2016-04-19 14:50:25 -0400

    Added methods in mapper and reducer

--- src/main/java/com/mr/Reducer.java
+++ src/main/java/com/mr/Reducer.java
@@ -1,11 +1,11 @@
 package com.mr;
 
+import com.main.Context;
+
 public abstract class Reducer {
 	
 	public abstract void setup();
-	
-	public abstract void reduce();
-		
+	public abstract <T> void reduce(String key, Iterable<T> value, Context context);
 	public abstract void cleanup();
 
 }
