28b088f8866c492bce2b54464f8b45ee82db4b7e
mehhtadeepen <mehta.de@husky.neu.edu>
2016-04-19 21:28:35 -0400

    added context and examples

---
+++ src/main/java/com/examples/R.java
@@ -0,0 +1,26 @@
+package com.examples;
+
+import com.main.Context;
+import com.mr.Reducer;
+
+public class R extends Reducer{
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T> void reduce(String key, Iterable<T> value, Context context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
